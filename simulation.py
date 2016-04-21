e0dcda430331ef04cff8ec576ba71bcd16e0f746
Ian Paul <ian.paul@students.olin.edu>
2016-04-21 10:05:37 -0400

    added a gate class

--- simulation.py
+++ simulation.py
@@ -12,17 +12,31 @@ class Qubit(object):
         self.phi = math.atan2(math.sqrt(x**2 + y**2), math.sqrt(x**2))
         self.alpha = math.cos(self.theta/2.)
         self.beta = (math.e**(1j*self.phi))*math.sin(self.theta/2.)
+        # zero  probablility is alpha^2
+        # one probability is beta ^2
 
     def check(self):
         if abs(self.alpha)**2 + abs(self.beta)**2 == 1:
             print "Good spin"
+            print self.x, self.y, self.z
+            print self.theta, self.phi
+            print self.alpha, self.beta
             return True
         else:
             print "Bad spin"
             return False
 
+class Gate(object):
+    '''Base class to define other gates from,
+    Matrix needs to be numpy matrix'''
+    def __init__(self, matrix):
+        self.operation = matrix
+    
+
+
+
 def test():
-    testBit = Qubit(1,0,0)
+    testBit = Qubit(0,1,0)
     testBit.check()
 
 if __name__ == '__main__':
