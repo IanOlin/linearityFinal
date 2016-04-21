89e78c507f86d298b01786a5f60e52fcaf913309
Ian Paul <ian.paul@students.olin.edu>
2016-04-21 09:22:32 -0400

    pushing for Katie

--- simulation.py
+++ simulation.py
@@ -2,5 +2,11 @@ import math
 import numpy as np 
 
 class Qubit(object):
-    '''This defines a qubit in our simulation'''
+    '''This defines a qubit in our simulation, we will be using a spin-(1/2) particle
+    spin is defined as (alpha)|0> + (beta)|1> '''
+    def __init__(self, name, alpha, beta)
+    self.name = name
+    self.alpha = alpha
+    self.beta = beta
+
     
